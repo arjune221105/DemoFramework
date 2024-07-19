6e57d85311704a3bd524da8aec3fd5330cbb8478
arjune221105 <shakti221105@gmail.com>
2024-07-18 20:56:03 -0700

    allworingfine

---
+++ webshop101/src/test/java/webshop101/usertext.java
@@ -0,0 +1,10 @@
+package webshop101;
+
+public class usertext {
+	
+	public static void main(String[] args) {
+		System.out.println("User Name are showinng");
+		System.out.println("Webshop name are showing");
+	}
+
+}
